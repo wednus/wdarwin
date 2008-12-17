99453556df4069a93974be3369150cb634f370a6
sundew@wednus.com <sundew@wednus.com@077981a8-a251-0410-9698-eb9d71b44f4c>
2008-12-17 13:02:41 +0000

    minor updates maintenance commit

--- trunk/wednus_darwin/module/darwin/Skill.js
+++ trunk/wednus_darwin/module/darwin/Skill.js
@@ -17,35 +17,30 @@ W.Skill = function(args){var self = this;
   // default action
   this.name = 'hide';
   this.img = 'module/darwin/image/blank.gif';
+	this.repeat = false;
   this.east = '0:0';
   this.west = '0:0';
   this.north = '0:0';
   this.south = '0:0';
-
   this.dir = 'south';  // current direction
   this.frame = 0;  // current frame
-
   // place holder for creature which this skill added
   this.creature = {getAbsDir: new Function()};
   // do this skiil toward the given direction
   this.toward = function(dir){
     self.creature.dir = self.creature.getAbsDir(dir);
   }
-
   // extend/override constructor w/ passed args object
   for(var i in args)
     eval('this.'+ i +' = args["'+ i +'"];');
-
   this.east = splitter(this.east);
   this.west = splitter(this.west);
   this.north = splitter(this.north);
   this.south = splitter(this.south);
-
   // split by comma
   function splitter(arg){
 		if(typeof arg.split == 'undefined')
 		  return arg;
-
     var tmp = arg.split(',');
     var arr = [];
     for(var i = 0; i < tmp.length; ++i){
@@ -61,10 +56,6 @@ W.Skill = function(args){var self = this;
 
 /**
  * animate skill
- * @test <a href='../../test/animation_reymond.html'>animate creature (1-frame: animation gif)</a>
- * @test <a href='../../test/animation.html'>animate creature (3-frame)</a>
- * @test <a href='../../test/animation_villager.html'>animate creature (4-frame)</a>
- * @test <a href='../../test/animation_rock_cn_sprites.html'>animate creature (6-frame)</a>
  * @test <a href='../../test/animation_lemming.html'>animate creature (8-frame)</a>
  */
 W.Skill.prototype.animate = function(){
@@ -72,7 +63,6 @@ W.Skill.prototype.animate = function(){
   if(this.frame >= this[this.dir].length - 1){
     this.frame = 0;
   }else ++this.frame;
-  
   //window.status = 'dir:'+ this.creature.dir +', this.frame:'+ this.frame;
   // change sprite image only if its needed
   if(this.creature.img.src != W.path + this.img)
